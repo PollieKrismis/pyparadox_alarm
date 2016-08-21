0156b818e86ef0aebc69369da106d3016a3a9f99
PollieKrismis <PollieXmas@icloud.com>
2016-08-21 13:23:23 +0200

    committing defaults

---
+++ paradox/defaults.py
@@ -0,0 +1,15 @@
+
+PARADOX_MODELS = {
+    'EVO48' :
+    {'Model' : 'EVO48', 'MaxZones' : 12, 'MaxAreas' : 4, 'MaxUsers' : 6, 'Baudrate' : 57600},
+    'EVO96' :
+    {'Model' : 'EVO96', 'MaxZones' : 96, 'MaxAreas' : 8, 'MaxUsers' : 999, 'Baudrate' : 57600},
+    'EVO192' :
+    {'Model' : 'EVO192', 'MaxZones' : 192, 'MaxAreas' : 8, 'MaxUsers' : 999, 'Baudrate' : 57600},
+    'DGP-848' :
+    {'Model' : 'DGP-848', 'MaxZones' : 48, 'MaxAreas' : 4, 'MaxUsers' : 96, 'Baudrate' : 57600},
+    'DGP-NE96' :
+    {'Model' : 'DGP-NE96', 'MaxZones' : 96, 'MaxAreas' : 8, 'MaxUsers' : 999, 'Baudrate' : 57600}
+    }
+
+
