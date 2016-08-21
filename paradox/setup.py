1f86e2f61565c16bbd2313e4c0d4539e18d36ec3
PollieKrismis <PollieXmas@icloud.com>
2016-08-21 14:50:15 +0200

    Adding setup.py

---
+++ paradox/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyparadox-alarm',
+      version='0.0.1',
+      description='Paradox Interface Library and Server',
+      author='Paul Burger',
+      author_email='PollieXmas@icloud.com',
+      url='https://github.com/PollieKrismis/pyparadox-alarm.git',
+      packages=['paradox'],
+      install_requires=['pyserial'],
+      scripts=[],
+  )
