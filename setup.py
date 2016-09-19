74fef8c76630a9b2efe104298921cd68295cf078
PollieKrismis <PollieXmas@icloud.com>
2016-09-19 22:06:59 +0200

    had to rename pyparadox to pyparadox_alarm

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='pyparadox',
+setup(name='pyparadox_alarm',
       version='0.1.2',
       description='A python3 library for running asynchronous communications with Paradox alarm control panel modules.',
       author='Paul Burger',
       author_email='PollieXmas@icloud.com',
-      url='https://github.com/PollieKrismis/pyparadox.git',
+      url='https://github.com/PollieKrismis/pyparadox_alarm.git',
       license='MIT',
-      packages=['pyparadox'],
+      packages=['pyparadox_alarm'],
       install_requires=['pyserial'],
       classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3.4','Programming Language :: Python :: 3.5']
   )
